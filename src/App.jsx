1f0140f9e70505c6d037e0452981b3b91934ad60
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-03-04 01:45:28 +0200

    Create: Components Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -13,7 +11,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
