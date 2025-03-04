a92bcaa4bd691a374148f3eeb3270a0402a87e5a
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-03-05 01:59:06 +0200

    Create: Images & Icons Folder

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
 
 export function Header() {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
   return (
     <>
-      <header>
-        <div className="container">
+      <header className=" py-5">
+        <div className=" w-[90%] m-auto flex items-center justify-between text-white ">
           <div className="logo">
-            <Link to="/" className="">
+            <Link to="/" className=" font-semibold text-[40px] ">
               Dark Weather
             </Link>
           </div>
+          <nav>
+            <ul>
+              <li>
+                <Link className=" text-2xl font-medium ">Home</Link>
+              </li>
+              <li>
+                <Link className="text-2xl font-light ">Download App</Link>
+              </li>
+              <li>
+                <Link className="text-2xl font-light ">Contact us</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </header>
     </>
